ecf3cf6317dc4da3f17bad35c0ea926618736542
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-07-01 15:30:14 -0600

    create a junior csv and a senior csv

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const { setGlobalSpreadsheetId,
     setGlobalAuthClient,
+    setGlobalStartDate,
     getNewToken,
-    handleInboundAuthRedirect } = require('../lib/googleApiClient');
+    handleInboundAuthRedirect,
+    getSpreadsheetValues,
+    sendCsv } = require('../lib/googleApiClient');
+const { makeCsv } = require('../lib/csv')
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/search', setGlobalSpreadsheetId, setGlobalAuthClient, getNewToken)
+app.get('/search', setGlobalSpreadsheetId, setGlobalStartDate, setGlobalAuthClient, getNewToken)
 
-app.get('/auth', handleInboundAuthRedirect)
+app.get('/auth', handleInboundAuthRedirect, getSpreadsheetValues, makeCsv, sendCsv)
 
 app.use(function (req, res, next) {
     next({ status: 404, message: "No routes found" })
