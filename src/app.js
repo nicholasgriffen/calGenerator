8ed2d9f040f4f5b7da35477e5e272af40e6e506b
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-06-30 19:05:59 -0600

    retrieve a spreadsheet

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const api = require('./googleApiClient');
+const api = require('../lib/googleApiClient');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/', api.setGlobalAuthClient, api.getNewToken)
 
-// Google API will redirect to this URL 
-// with a query param code
 app.get('/auth', api.handleInboundAuthRedirect)
 
 app.use(function (req, res, next) {
